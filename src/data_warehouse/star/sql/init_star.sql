51e5abed4bcd69c343bba4eadf5e7239ade7bc4b
Aleksandr <aleksandr.e.gusev@gmail.com>
2025-12-25 16:51:58 +0300

    update star scheme with hash

--- src/data_warehouse/star/sql/init_star.sql
+++ src/data_warehouse/star/sql/init_star.sql
@@ -1,5 +1,7 @@
 create table if not exists customer_dim (
-    customer_id bigserial PRIMARY KEY,
+    customer_hash varchar(255) unique,
+    customer_id bigint,
+    customer_sk bigserial PRIMARY KEY,
     name varchar(255) not null,
     email varchar(255) not null,
     phone varchar(255) not null,
@@ -9,7 +11,9 @@ create table if not exists customer_dim (
 );
 
 create table if not exists product_dim (
-    product_id bigserial PRIMARY KEY,
+    product_hash varchar(255) unique,
+    product_id bigint,
+    product_sk bigserial PRIMARY KEY,
     product_name varchar(255) not null,
     category varchar(255) not null,
     start_date DATE not null,
@@ -17,22 +21,11 @@ create table if not exists product_dim (
     is_current BOOLEAN DEFAULT true
 );
 
-create table if not exists date_dim (
-    date_id bigserial primary key,
-    date_value timestamp with time zone not null unique,
-    year int not null generated always as (EXTRACT(YEAR FROM date_value AT TIME ZONE 'UTC')) stored,
-    month int not null generated always as (EXTRACT(MONTH FROM date_value AT TIME ZONE 'UTC')) stored,
-    day int not null generated always as (EXTRACT(DAY FROM date_value AT TIME ZONE 'UTC')) stored,
-    hour int not null generated always as (EXTRACT(HOUR FROM date_value AT TIME ZONE 'UTC')) stored,
-    minute int not null generated always as (EXTRACT(MINUTE FROM date_value AT TIME ZONE 'UTC')) stored,
-    second int not null generated always as (EXTRACT(SECOND FROM date_value AT TIME ZONE 'UTC')) stored
-);
-
 create table if not exists sales_fact (
-    sale_id bigserial PRIMARY KEY,
-    customer_id bigint REFERENCES customer_dim(customer_id) not null,
-    product_id bigint REFERENCES product_dim(product_id) not null,
-    date_id bigint REFERENCES date_dim(date_id) not null,
+    sale_id bigint,
+    customer_sk bigint,
+    product_sk bigint,
+    quantity integer not null check (quantity >= 1),
     amount integer not null check (amount >= 1),
-    quantity integer not null check (quantity >= 1)
+    date_value timestamp with time zone not null
 );
