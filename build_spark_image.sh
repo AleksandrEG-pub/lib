d8782f863114b357fe793efa2f35444762ef9ef8
Aleksandr <aleksandr.e.gusev@gmail.com>
2026-01-25 11:46:03 +0000

    improve spark dockerfile and docker service

--- build_spark_image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -t spark-image -f Dockerfile.spark .
--- start-spark.sh
+++ start-spark.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-./build_spark_image.sh && \
 docker compose --file ./docker-compose.yaml up -d spark-master spark-worker
